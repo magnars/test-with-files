0531b1be0116d787443025235382b685d03d49a9
Christopher Mark Gore <cgore@cgore.com>
2016-01-20 12:45:30 -0600

    Updating dependencies.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/magnars/test-with-files"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.0.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :profiles {:dev {:dependencies [[midje "1.8.3"]]
+                   :plugins [[lein-midje "3.2"]]
                    :resource-paths ["test/resources"]}})
