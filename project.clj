526c94a6ac4cf8c715167283a00ffef1e2745007
Wes Morgan <git@wesmorgan.me>
2023-01-31 11:08:09 -0700

    Update deps to latest compatible versions

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies []
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.1"]
-                                  [lambdaisland/kaocha "1.0.732"]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.11.1"]
+                                  [lambdaisland/kaocha "1.76.1230"]
                                   [kaocha-noyoda "2019-06-03"]]
                    :resource-paths ["test/resources"]}}
   :aliases {"kaocha" ["run" "-m" "kaocha.runner"]})
